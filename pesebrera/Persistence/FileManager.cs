2657bb0d45d6296930703f9b56c8029650103119
Juan Diego Gonzalez <juanx96506@gmail.com>
2019-02-05 22:37:03 -0500

    Added FileManager Class

---
+++ pesebrera/Persistence/FileManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace pesebrera.Persistence
+{
+    public class FileManager
+    {
+        private string _filePath;
+
+        public FileManager(string filePath)
+        {
+            if (!string.IsNullOrEmpty(filePath))
+                _filePath = filePath;
+            else
+                throw new ArgumentException("Dirección del archivo invalida", "filePath");
+        }
+
+        public string ReadFile()
+        {
+            string readText = File.ReadAllText(_filePath);
+            return readText;
+        }
+
+        public void WriteFile(string newText)
+        {
+            File.WriteAllText(_filePath, newText);
+        }
+    }
+}
