e4008b1d4278c96764bca87f651d046c313e8241
Juan Diego Gonzalez <juanx96506@gmail.com>
2019-02-07 23:51:14 -0500

    Inserter test of 'ExecuteClassification' Method

---
+++ pesebreraTests/Business/AnimalClassifierTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pesebrera.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pesebrera.Business.Tests
+{
+    [TestClass()]
+    public class AnimalClassifierTests
+    {
+        [TestMethod()]
+        public void ExecuteClassificationTest()
+        {
+            AnimalClassifier classifier = new AnimalClassifier();
+            IAnimal[] animals = classifier.ExecuteClassification();
+            Assert.IsNotNull(animals);
+            int numberOfBovines = animals.Where(x => x.Family is Bovine).Count();
+            Assert.AreEqual(numberOfBovines, 4);
+        }
+    }
+}
